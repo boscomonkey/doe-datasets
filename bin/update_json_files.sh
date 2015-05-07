d0a04f44cb23c6ae8ba7b7c2a5801698116c0b97
Bosco So <boscoso@halcyon.local>
2015-05-07 11:33:41 -0400

    update script also creates local copy of CSV

--- bin/update_json_files.sh
+++ bin/update_json_files.sh
@@ -3,5 +3,5 @@
 # Convenience utility to update both JSON files from Google
 # Spreadsheet tabs.
 
-bin/categories_csv.sh | bin/csv_to_json.rb > data/categories.json
-bin/datasets_csv.sh   | bin/csv_to_json.rb > data/datasets.json
+bin/categories_csv.sh | tee data/categories.csv | bin/csv_to_json.rb > data/categories.json
+bin/datasets_csv.sh   | tee data/datasets.csv   | bin/csv_to_json.rb > data/datasets.json
